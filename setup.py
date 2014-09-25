4ef52d069240bd29fe7b3cd955bf7d6db014a34d
Nick Furlotte <nick.furlotte@gmail.com>
2014-09-24 19:54:28 -0700

    Script location

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 
 from distutils.core import setup
 
-#extra={'scripts':['bin/pylmmGWAS.py','pylmmKinship.py']},
-
 setup(
       name='pyLMM',
       version='0.99',
@@ -11,5 +9,5 @@ setup(
       url = "https://github.com/nickFurlotte/pylmm",
       description = 'pyLMM is a lightweight linear mixed model solver for use in GWAS.',
       packages=['pylmm'],
-      scripts=['bin/pylmmGWAS.py','pylmmKinship.py'],
+      scripts=['pylmmGWAS.py','pylmmKinship.py'],
     )
